a3fcbb74fa51c54113799f67cb392143e98e145c
Frank Amoroso <famoroso@ciena.com>
2019-04-03 06:40:24 -0700

    Use CDN to offload our server burden

--- src/router.js
+++ src/router.js
@@ -1,11 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import Home from './views/Home'
 import About from './views/About'
 
-Vue.use(Router)
-
-export default new Router({
+export default new VueRouter({
   routes: [{
     path: '/',
     name: 'home',
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,22 @@
-module.exports = {}
+module.exports = {
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined,
+  pluginOptions: {
+    externals: {
+      common: [{
+        id: 'vue',
+        assets: 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+        global: 'Vue'
+      }, {
+        id: 'vue-router',
+        assets: 'https://unpkg.com/vue-router@3.0.2/dist/vue-router.min.js',
+        global: 'VueRouter'
+      }]
+    }
+  }
+}
